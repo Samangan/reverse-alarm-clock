9fdd92bec0b8bcf5bb806ae356a1fe5496da99c8
Harry <harry.hull1@gmail.com>
2013-10-27 13:56:33 -0500

    added first tests

--- js/src/reverseAlarm.js
+++ js/src/reverseAlarm.js
@@ -12,13 +12,32 @@
 
 		//parse the prompt into hours, minutes, seconds (test this thoroughly)
 
-		//return minutes (test this thoroughly)
+		//return milliseconds (test this thoroughly)
 	};
 
-	reverseAlarm.startCountDown = function(minutes) {
+	reverseAlarm.startCountDown = function(milliseconds) {
 		//start the countdown to close the tab in minutes
 
 		//update the countdown (How can I test this)
+	};
+
+
+
+	reverseAlarm._parseStringToHoursMinsSecs = function(userInputtedString) {
+		//input: string
+		//output: Array[hours, mins, seconds]
+		var parsedTime = new Array(3);
+
+		//TODO: implement
+
+
+		return parsedTime;
+	};
+
+	reverseAlarm._convertToMilliseconds = function(parsedTimeArr) {
+		//input: Array[hours, mins, seconds]
+		//output: number of milliseconds from the accumulative time inputted
+		
 	};
 
 
--- js/test/reverseAlarm_test.js
+++ js/test/reverseAlarm_test.js
@@ -4,7 +4,34 @@ buster.testCase("testcase", {
 	setUp: function () {
 		var reverseAlarm = window.reverseAlarm;
 	},
-	"test": function () {		
+	"test": function () {
+		assert.match(1, 1);
+	}
+});
+
+
+buster.testCase("can parse input string into hours mins and seconds", {
+	setUp: function () {
+		var reverseAlarm = window.reverseAlarm;
+	},
+	"test can parse hours": function () {
+		//verbose input
+		var input_verbose = "4 hours";
+		
+		var arrOutput = reverseAlarm._parseStringToHoursMinsSecs(input_verbose);
+		assert.match(arrOutput[0], 4);
+
+		//minimal input ("4 h")
+
+		//normal input ("4 hrs" / "4 hr")
+
+	},
+
+	"test can parse minutes": function () {
+		assert.match(1, 1);
+	},
+
+	"test can parse seconds": function () {
 		assert.match(1, 1);
 	}
 });
